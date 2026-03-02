d2c0c6565ddea8492d4809d4af63e78ec5e95e7b
vinilana <viniciuslanadev@gmail.com>
2026-03-02 15:45:36 -0300

    add health check

--- apps/backend-nestjs/src/app.module.ts
+++ apps/backend-nestjs/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { FranchiseModule } from './franchise/franchise.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [DatabaseModule, AuthModule, FranchiseModule],
+  imports: [DatabaseModule, AuthModule, FranchiseModule, HealthModule],
 })
 export class AppModule {}
